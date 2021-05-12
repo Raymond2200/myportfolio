ded07d35158cb753658f68f46fe6232ec8542d34
raymond <raycasgrain@gmail.com>
2021-05-12 16:27:09 -0400

    icons for skills

--- port/src/components/About/About.jsx
+++ port/src/components/About/About.jsx
@@ -3,7 +3,21 @@ import './About.css'
 function About(props) {
     return (
         <div className="aboutBG">
-            <h1>About</h1>
+            <div>
+                <h1 className="aboutHead">About Me</h1>
+                <p className="aboutTxt">Hello, my name is Raymond a recent graduate from the General Assembly Software Development immersive course. I am a <strong>full-stack developer</strong> who is an avid learner and challenge seeker.</p>
+                <br/>
+                <p className="aboutTxt">My background originates in finance and the capital markets which helps give me a strong flexibility when working with customer needs, as well as technical requirements. My transition to a software developer was initiated by me need to learn and be challenged. To that end I am constantly bringing my best and always seeking to improve his best. I am a question asker, but one that seeks to take his own initiative.</p>
+                <br/>
+                <p className="aboutTxt">I am currently completing a python machine learning course on Udemy and will be developing my skills with react native in the near future.</p>
+            </div>
+            <h2 className="aboutStr">Strong Skills</h2>
+            <div>
+                <div className="skillContainer"> <img src="https://img.icons8.com/color/96/000000/html-5--v2.png"/><p className="skillTxt">JavaScript</p></div>
+                <div className="skillContainer"> <img src="https://img.icons8.com/color/96/000000/python.png"/><p className="skillTxt">Python 3</p></div>
+                <div className="skillContainer"> <img src="https://img.icons8.com/color/96/000000/nodejs.png"/><p className="skillTxt">Node.js</p></div>
+                <div className="skillContainer"> <img src="icons\html.svg"/><p className="skillTxt">HTML5</p></div>
+            </div>
         </div>
     )
 }
--- port/src/components/Me/Me.jsx
+++ port/src/components/Me/Me.jsx
@@ -3,11 +3,12 @@ import './Me.css'
 function Me(props) {
     return(
         <div className="meBG">
-            <div className="meCover">
-                <div >
-                    <h1 className="meTxt">Raymond <br/>&nbsp;&nbsp;&nbsp;&nbsp; Casgrain </h1>
-                    <h3 className="meTxt">def FULL(STACK): <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; return DEVELOPER</h3>
+            <div className="meCon">
+                <div className="meCover">
+                    <h1 className="meName">Raymond <br/>&nbsp;&nbsp;&nbsp;&nbsp; Casgrain </h1>
+                    <h2 className="meTxt">&#60; FULL-STACK DEVELOPER &gt;</h2>
                 </div>
+                <img src="https://i.imgur.com/ePKqZIB.jpg" className="meimg"/>
             </div>
         </div>
     )
