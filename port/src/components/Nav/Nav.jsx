6bc924ba9b1bc13e902bb8e03220b2ffaccf0824
raymond <raycasgrain@gmail.com>
2021-05-27 18:11:16 -0400

    current portfolio

--- port/src/components/Nav/Nav.jsx
+++ port/src/components/Nav/Nav.jsx
@@ -1,15 +1,19 @@
 import './Nav.css';
-import {scrollToElement} from "react-scroll-hooks";
 
 function Nav(props) {
+    
+    let mailTo = () => {
+        window.open("mailto:raycasgrain@gmail.com")
+    }
 
     return (
         <div className="nav">
             <ul>
                 <li><a href="https://github.com/Raymond2200"><img className="navImg" src="https://i.imgur.com/Vu0sWdk.png" target="_blank"/></a></li>
-                <li className="linked">About</li>
+                {/* <li className="linked">About</li>
                 <li className="linked">Projects</li>
-                <li className="linked">Contact</li>
+                <li className="linked">Contact</li> */}
+                <li className="linked" onClick={mailTo}> Contact Me</li>
                 <li><a href="https://www.linkedin.com/in/raymondcasgrain/"><img className="navImg2" src="https://i.imgur.com/JtxJz8Y.png" target="_blank"/></a></li>
             </ul>
         </div>
--- port/src/pages/App/App.js
+++ port/src/pages/App/App.js
@@ -3,29 +3,19 @@ import Nav from '../../components/Nav/Nav';
 import Me from '../../components/Me/Me';
 import About from '../../components/About/About';
 import Projects from '../../components/Projects/Projects';
-import Footer from '../../components/Footer/Footer';
-import {useRef} from "react";
-import useScrollPosition from '@react-hook/window-scroll';
-
-
-
 
 function App() {
-  let contRef = useRef()
-
-  let position0 = () => goToPosition(0)
 
   return (
     <div className="App">
+      <Nav className="navMain"/>
       <div className="main">
-        <Nav className="navMain"/>
         <Me className="meMain"/>
       </div>
-      <About className="aboutMain"></About>
+      <About className="aboutMain"/>
       <div className="main2">
         <Projects />
       </div>
-      <Footer/>
     </div>
   );
 }
